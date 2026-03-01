471e6fe87468a1cf0a1692f3e69b736f22f4ebb4
ThreeManager785 <leimeichun50@gmail.com>
2026-03-01 14:02:49 +1000

    Initial Commit

---
+++ GreatSekai/GreatSekaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreatSekaiApp.swift
+//  GreatSekai
+//
+//  Created by ThreeManager785 on 2026/3/1.
+//
+
+import SwiftUI
+
+@main
+struct GreatSekaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
